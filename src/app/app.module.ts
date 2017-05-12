7adde8640ee2865b0873f26dc1ce048d4fb0d199
Muron <hectop23@ukr.net>
2017-05-12 15:35:30 +0300

    backbone for project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import {RouterModule} from '@angular/router';
+import {routes} from './app.routes';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './admin/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
