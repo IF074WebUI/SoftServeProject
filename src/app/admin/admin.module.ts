6e5efcfba2d3c3d9a3ebda90b05e1ff13c19b9b7
Gipotalamus <hectop23@ukr.net>
2017-05-15 22:24:30 +0300

    child routes fixed, login component mergeMap fixed

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,8 +3,9 @@ import {RouterModule} from '@angular/router';
 import {adminRoutes} from './admin.routes';
 import {AdminComponent} from './admin.component';
 import {MenuComponent} from './menu/menu.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 @NgModule({
-  declarations: [AdminComponent, MenuComponent],
+  declarations: [AdminComponent, MenuComponent, StatisticsComponent],
   imports: [RouterModule.forChild(adminRoutes)]
 })
 export class AdminModule {
--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -1,5 +1,10 @@
 import {Route} from '@angular/router';
 import {AdminComponent} from './admin.component';
+import {StatisticsComponent} from "./statistics/statistics.component";
 export const adminRoutes: Route[] = [
-  {path: '', component: AdminComponent, pathMatch: 'full'}
+  {path: '', component: AdminComponent, children: [
+    /*тут, в дітях вписуємо свої роути*/
+    {path: 'statistics', component: StatisticsComponent}
+  ]},
+
 ];
