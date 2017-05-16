17a530b59cbe45219e21f1688c04f0db11259a98
andrijdovbenko <andrijdovbenko@gmail.com>
2017-05-16 12:00:09 +0300

    some changes before marge

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -1,6 +1,5 @@
 import {Route} from '@angular/router';
 import {AdminComponent} from './admin.component';
-import {StatisticsComponent} from "./statistics/statistics.component";
 import {GroupComponent} from "./group/group.component";
 export const adminRoutes: Route[] = [
   {
@@ -8,6 +7,5 @@ export const adminRoutes: Route[] = [
     children: [
       /*тут, в дітях вписуємо свої роути*/
       {path: 'group', component: GroupComponent},
-      {path: 'statistics', component: StatisticsComponent}
     ]
   }];
