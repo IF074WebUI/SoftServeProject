2e94e266fffb16d8d279747fac503a0caa8d0e73
Elena Taras <lena@Elenas-iMac.lan>
2017-05-17 16:32:47 +0300

    added Faculties entity

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,10 +6,11 @@ import {MenuComponent} from './menu/menu.component';
 import { GroupComponent } from './group/group.component';
 import {StatisticsComponent} from "./statistics/statistics.component";
 import {StatisticsService} from './statistics/statistics.service';
+import { FacultiesComponent } from './faculties/faculties.component';
 
 
 @NgModule({
-  declarations: [AdminComponent, MenuComponent, StatisticsComponent, GroupComponent],
+  declarations: [AdminComponent, MenuComponent, StatisticsComponent, GroupComponent, FacultiesComponent],
   imports: [RouterModule.forChild(adminRoutes)],
   providers: [StatisticsService]
 
--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -2,12 +2,14 @@ import {Route} from '@angular/router';
 import {AdminComponent} from './admin.component';
 import {GroupComponent} from "./group/group.component";
 import {StatisticsComponent} from './statistics/statistics.component';
+import {FacultiesComponent} from './faculties/faculties.component';
 export const adminRoutes: Route[] = [
   {
     path: '', component: AdminComponent,
     children: [
       /*тут, в дітях вписуємо свої роути*/
       {path: 'group', component: GroupComponent},
-      {path: 'statistics', component: StatisticsComponent}
+      {path: 'statistics', component: StatisticsComponent},
+      {path: 'faculties', component: FacultiesComponent}
     ]
   }];
