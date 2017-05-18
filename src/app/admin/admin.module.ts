8489686f92c6733c62e9e1d9d1175633f3ac93b1
Gipotalamus <hectop23@ukr.net>
2017-05-18 23:27:01 +0300

    new specialities functionality added (common entities component, component for pagination); not completed yet second try

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,12 +7,25 @@ import { GroupComponent } from './group/group.component';
 import {StatisticsComponent} from "./statistics/statistics.component";
 import {StatisticsService} from './statistics/statistics.service';
 import { FacultiesComponent } from './faculties/faculties.component';
+import { SpecialitiesComponent } from './specialities/specialities.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { CommonComponent } from './common/common.component';
+import {SpecialitiesService} from "./services/specialities.service";
+import {CommonModule} from "@angular/common";
 
 
 @NgModule({
-  declarations: [AdminComponent, MenuComponent, StatisticsComponent, GroupComponent, FacultiesComponent],
-  imports: [RouterModule.forChild(adminRoutes)],
-  providers: [StatisticsService]
+  declarations: [
+    AdminComponent,
+    MenuComponent,
+    StatisticsComponent,
+    GroupComponent,
+    FacultiesComponent,
+    SpecialitiesComponent,
+    PaginationComponent,
+    CommonComponent],
+  imports: [RouterModule.forChild(adminRoutes), CommonModule],
+  providers: [StatisticsService, SpecialitiesService]
 
 })
 export class AdminModule {}
---
+++ src/app/admin/pagination/pagination.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dtester-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.css']
+})
+export class PaginationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
