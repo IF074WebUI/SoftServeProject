520fc07b0f0a46668ee1a3caf0dcced9d56e6f49
Elena Taras <elena_taras@ukr.net>
2017-05-23 20:14:41 +0300

    routing by id, validation done

--- src/app/admin/faculties/faculties.component.ts
+++ src/app/admin/faculties/faculties.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {Faculty} from './Faculty';
 import {FacultyService} from './faculty.service';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {FormControl, FormGroup, Validators, AbstractControl} from '@angular/forms';
-import {Subscription} from 'rxjs/Subscription';
-import { ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Location} from '@angular/common';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-faculties',
@@ -14,33 +15,44 @@ import { ActivatedRoute} from '@angular/router';
 })
 export class FacultiesComponent implements OnInit {
   faculties: Faculty[] = [];
-  selecfaculties: Faculty;
   page: number = 1;
   count: number;
   ItemforEdit: Faculty;
   ItemforDelete: Faculty;
   faculty: Faculty = new Faculty();
-  facultyForm: FormGroup;
-  facultyName: FormControl;
-  facultyDescription: FormControl;
-  array: Array<number>;
-  private id: number;
-  private subscription: Subscription;
+  facultyEditForm: FormGroup;
+  facultyEditName: FormControl;
+  facultyEditDescription: FormControl;
+  facultyEditId: FormControl;
+  facultyAddForm: FormGroup;
+  facultyAddName: FormControl;
+  facultyAddDescription: FormControl;
+  modalHeader: string;
+  list: Array<number> = [];
 
-  constructor(private http: FacultyService, private modalService: NgbModal, private activateRoute: ActivatedRoute ) {
-    this.subscription = activateRoute.params.subscribe(params=>this.id=params['id']);
-    this.http.getFacultyById(this.id).subscribe((resp) => {
-      this.selecfaculties = resp;
-      console.log(this.selecfaculties);  })
-}
+
+  constructor(private http: FacultyService, private modalService: NgbModal,
+              private route: ActivatedRoute,
+              private location: Location) {
+  }
 
   ngOnInit() {
-    this.facultyName = new FormControl('', Validators.required, this.asyncValidator.bind(this));
-    this.facultyDescription = new FormControl('', Validators.required);
-    this.facultyForm = new FormGroup({
-      'name': this.facultyName,
-      'description': this.facultyDescription
+    this.facultyEditName = new FormControl('', Validators.required);
+    this.facultyEditDescription = new FormControl('', Validators.required);
+    this.facultyEditId = new FormControl('');
+    this.facultyEditForm = new FormGroup({
+      'id': this.facultyEditId,
+      'name': this.facultyEditName,
+      'description': this.facultyEditDescription
     })
+
+    this.facultyAddName = new FormControl('', Validators.required, this.asyncValidator.bind(this));
+    this.facultyAddDescription = new FormControl('', Validators.required);
+    this.facultyAddForm = new FormGroup({
+      'name': this.facultyAddName,
+      'description': this.facultyAddDescription
+    })
+
     this.http.getPaginatedPage(1).subscribe((resp) => {
       this.faculties = <Faculty[]> resp;
     })
@@ -48,8 +60,11 @@ export class FacultiesComponent implements OnInit {
     this.http.countAllRecords().subscribe((resp) => {
       this.count = resp['numberOfRecords'];
     });
-  }
 
+    this.route.params
+      .switchMap((params: Params) => this.http.getFacultyById(+params['id']))
+      .subscribe(resp => this.faculties = resp);
+  }
 
   getCount() {
     this.http.countAllRecords().subscribe((resp) => {
@@ -76,7 +91,6 @@ export class FacultiesComponent implements OnInit {
     this.ItemforDelete = faculty;
   }
 
-
   confirmDelete() {
     this.http.deleteItem(this.ItemforDelete['faculty_id']).subscribe((resp) => {
       this.getCount();
@@ -86,25 +100,24 @@ export class FacultiesComponent implements OnInit {
   }
 
   confirmEdit() {
-    this.http.editItem(this.ItemforEdit['faculty_id'], this.ItemforEdit['faculty_name'], this.ItemforEdit['faculty_description']).subscribe((resp) => {
+    this.http.editItem(this.facultyEditId.value, this.facultyEditName.value, this.facultyEditDescription.value).subscribe((resp) => {
       this.uploadAllPages(this.page);
     })
   }
 
   confirmAdd() {
-    this.http.addItem(this.facultyName.value, this.facultyDescription.value).subscribe(response => {
+    console.log(this.facultyAddName.value);
+    this.http.addItem(this.facultyAddName.value, this.facultyAddDescription.value).subscribe(response => {
       this.getCount();
       (this.count % 10 === 0) ? this.page = this.page + 1 : this.page = this.page;
       this.uploadAllPages(this.page);
-      this.faculty.name = "";
-      this.faculty.description = "";
     })
   }
 
   add(content) {
- //   this.facultyName.setValue("hi");
- //   this.facultyName.setValue("hello");
+    this.modalHeader = 'Створення нового факультету';
     this.modalService.open(content).result.then((result) => {
+      console.log(this.facultyAddName.value);
       this.confirmAdd();
     }, (reason) => {
       console.log(`Dismissed`);
@@ -120,18 +133,21 @@ export class FacultiesComponent implements OnInit {
     });
   }
 
+
+
   edit(content) {
-    let n = this.ItemforEdit['faculty_name'];
-    let d = this.ItemforEdit['faculty_description'];
+    this.modalHeader = 'Редагування факультету';
+    this.facultyEditId.setValue(this.ItemforEdit['faculty_id']);
+    this.facultyEditName.setValue(this.ItemforEdit['faculty_name']);
+      this.facultyEditDescription.setValue(this.ItemforEdit['faculty_description']);
     this.modalService.open(content).result.then((result) => {
       this.confirmEdit();
       alert('Факультет було успішно відредаговано');
     }, (reason) => {
-      this.ItemforEdit['faculty_name'] = n;
-      this.ItemforEdit['faculty_description'] = d;
       console.log(`Dismissed`);
     });
   };
+
 
   asyncValidator(control: AbstractControl) {
     return this.http.searchByName(control.value).map((resp: Faculty[]) => {
@@ -144,6 +160,11 @@ export class FacultiesComponent implements OnInit {
       }
     )
   }
+
+ // PaginationList(){
+//    this.list.push(this.count/10 + 1);
+ //   return this.list;
+ // }
 
 
 }
