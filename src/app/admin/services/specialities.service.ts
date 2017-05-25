1e543ef4911d13c4db73cbbb0bc2fa9a7c5e367f
Gipotalamus <hectop23@ukr.net>
2017-05-26 00:18:25 +0300

    constants added

--- src/app/admin/services/specialities.service.ts
+++ src/app/admin/services/specialities.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { Http, RequestOptions, Response, Headers } from '@angular/http';
 import { Observable } from 'rxjs';
 import { Speciality } from '../specialities/speciality';
-import { HOST } from '../../constants';
+import {SPECIALITY_URI} from '../../constants';
 
 @Injectable()
 export class SpecialitiesService {
 
   options: RequestOptions;
+
 
   constructor(private http: Http) {
     const headers: Headers = new Headers({'Content-Type': 'application/json'});
@@ -15,32 +16,32 @@ export class SpecialitiesService {
   }
 
   getAll(): Observable<Speciality[]> {
-    return this.http.get(`http://${HOST}/speciality/getRecords`).map((resp: Response) => resp.json());
+    return this.http.get(`${SPECIALITY_URI}getRecords`).map((resp: Response) => resp.json());
   }
 
   getPaginated(limit: number, offset: number): Observable<Speciality[]> {
-  return this.http.get(`http://${HOST}/speciality/getRecordsRange/${limit}/${offset}`).map((resp: Response) => resp.json());
+  return this.http.get(`${SPECIALITY_URI}/getRecordsRange/${limit}/${offset}`).map((resp: Response) => resp.json());
   }
 
   searchByName(criteria: string): Observable<Speciality[]> {
-    return this.http.get(`http://${HOST}/speciality/getRecordsBySearch/${criteria}`).map((resp: Response) => resp.json());
+    return this.http.get(`${SPECIALITY_URI}/getRecordsBySearch/${criteria}`).map((resp: Response) => resp.json());
   }
 
   getCount(): Observable<number> {
-    return this.http.get(`http://${HOST}/speciality/countRecords`).map((resp: Response) => resp.json()['numberOfRecords']);
+    return this.http.get(`${SPECIALITY_URI}/countRecords`).map((resp: Response) => resp.json()['numberOfRecords']);
   }
 
   delete(id: number): Observable<Speciality> {
-    return this.http.delete(`http://${HOST}/speciality/del/${id}`).map((resp: Response) => resp.json());
+    return this.http.delete(`${SPECIALITY_URI}/del/${id}`).map((resp: Response) => resp.json());
   }
 
   edit(speciality: Speciality): Observable<Speciality> {
-    return this.http.post(`http://${HOST}/speciality/update/${speciality['speciality_id']}`, JSON.stringify(speciality), this.options)
+    return this.http.post(`${SPECIALITY_URI}/update/${speciality['speciality_id']}`, JSON.stringify(speciality), this.options)
       .map((resp: Response) => resp.json());
   }
 
   save(speciality: Speciality): Observable<Speciality> {
-    return this.http.post(`http://${HOST}/speciality/insertData`, JSON.stringify(speciality), this.options)
+    return this.http.post(`${SPECIALITY_URI}/insertData`, JSON.stringify(speciality), this.options)
       .map((resp: Response) => resp.json());
   }
 }
--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -2,6 +2,7 @@ import { CanLoad, Route, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { LoginService } from '../login/login.service';
 import { Observable } from 'rxjs/Observable';
+import { LOGGED, ROLE_ADMIN, ROLE_STUDENT } from "../constants";
 
 @Injectable()
 export class AdminGuard implements CanLoad {
@@ -11,13 +12,13 @@ export class AdminGuard implements CanLoad {
   canLoad(route: Route): Observable<boolean> {
     return this.loginService.checkLogged().map(resp => {
       let logged: string = resp['response'];
-      if (logged !== 'logged') {
+      if (logged !== LOGGED) {
         return false;
       } else {
         let role: string = resp['roles'][1];
-        if (role === 'admin') {
+        if (role === ROLE_ADMIN) {
           return true;
-        } else if (role === 'students') {
+        } else if (role === ROLE_STUDENT) {
           this.router.navigate((['/denied']));
         }
         return false;
