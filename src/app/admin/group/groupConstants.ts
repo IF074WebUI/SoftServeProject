772d9447ea09e21f6d896da1a29856b93853ca08
Mykola <pereslop@gmail.com>
2017-05-29 12:10:06 +0300

    adding constants for group headers

---
+++ src/app/admin/group/groupConstants.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by student on 29.05.17.
+ */
+export const GROUPS_HEADERS: string[] = ['пор.номер', 'Назва групи', 'адміністрування'];
+export const IGNORE_PROPERTIES: string[] = ['group_id', 'faculty_id', 'speciality_id' ];
+export const GROUP_ENITY: string = '/Group';
+
