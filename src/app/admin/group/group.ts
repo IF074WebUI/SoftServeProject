90589f321dda4bfe476a47963ea46343a1b68bd9
Mykola <pereslop@gmail.com>
2017-06-06 13:50:04 +0300

    small merge changes

--- src/app/admin/group/group.ts
+++ src/app/admin/group/group.ts
@@ -3,7 +3,7 @@ public  group_id: number;
   public group_name: string;
   public faculty_id: number;
   public speciality_id: number;
-  constructor(group_id, group_name, faculty_id, speciality_id){
+  constructor(group_id, group_name, faculty_id, speciality_id) {
     this.group_id = group_id;
     this.group_name = group_name;
     this.faculty_id = faculty_id;
