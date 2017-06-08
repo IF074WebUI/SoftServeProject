e28e8617b2e81809d386bc48a39bbcec868f80c3
Mykola <pereslop@gmail.com>
2017-06-08 16:40:24 +0300

    addind and editung testDetail added

--- src/app/admin/universal/dynamic-form/config.ts
+++ src/app/admin/universal/dynamic-form/config.ts
@@ -162,7 +162,7 @@ export const TEST_DETAIL_CONFIG = [
     placeholder: 'Select an option'
   },
   {
-    type: 'select',
+    type: 'select_with_options',
     label: 'Кількіст завдань',
     name: 'tasks',
     options: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'],
