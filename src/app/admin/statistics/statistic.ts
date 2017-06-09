f0e41045dcb978131db65c79bf1aa24a9e02eb94
Mykola <pereslop@gmail.com>
2017-06-09 16:07:43 +0300

    new statistic skeleton added

---
+++ src/app/admin/statistics/statistic.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by student on 09.06.17.
+ */
+export class Statistic {
+  public name: string;
+  public descriptiion: string;
+  public count: string;
+  constructor(name, description, count) {
+    this.name = name;
+    this.descriptiion = description;
+    this.count = count;
+  }
+}
--- src/app/admin/statistics/statistics.component.ts
+++ src/app/admin/statistics/statistics.component.ts
@@ -1,23 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { StatisticsService } from './statistics.service';
-
+import { Statistic } from './statistic';
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent implements OnInit {
-  entities: string[] = ['speciality', 'group', 'subject', 'test', 'student', 'question'];
-  entitiesNumber = {};
+  entity: Statistic[];
 
-
-  constructor(private statistics: StatisticsService) { }
-
+  constructor(private statistics: StatisticsService) {
+    this.entity = [
+      {name: 'speciality', descriptiion: 'Спеціальність', count: ''},
+      {name: 'group', descriptiion: 'Групи', count: ''},
+      {name: 'subject', descriptiion: 'Предмети', count: ''},
+      {name: 'test', descriptiion: 'Тести', count: ''},
+      {name: 'student', descriptiion: 'Студенти', count: ''},
+      {name: 'question', descriptiion: 'Питання', count: ''}];
+  }
   ngOnInit() {
-    for (const entity of this.entities) {
-      this.statistics.getCountRecords(entity).subscribe((data) => {
-        this.entitiesNumber[entity] = data.numberOfRecords;
+    let i: number = 0;
+    for (let entity of this.entity) {
+      this.statistics.getCountRecords(entity['name']).subscribe((data) => {
+        console.log(entity['name'], data.numberOfRecords);
+        this.entity[i].count = data.numberOfRecords;
+        i ++;
       });
     }
+    console.log(this.entity);
   }
 }
