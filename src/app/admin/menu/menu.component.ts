d46bedf5a3422514662a7993db6fe3de4e4b1986
ivasenko <ivasenko.iren@gmail.com>
2017-06-23 14:43:35 +0300

    without menu

--- src/app/admin/menu/menu.component.ts
+++ src/app/admin/menu/menu.component.ts
@@ -1,43 +1,17 @@
 import {Component, OnInit} from '@angular/core';
 import {LoginService} from "../../login/login.service";
 import {Router} from "@angular/router";
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  keyframes,
-  group
-} from '@angular/animations';
-
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css'],
-  animations: [
-    trigger('moveLeftRight', [
-      state('left', style({
-        transform: 'translateX(-100%)'
-      })),
-      state('right', style({
-        transform: 'translateX(100%)'
-      })),
-      transition('left => right', [animate('0.5s')]),
-      transition('right => left', [animate('0.5s')])
-        ])
-  ]
+  styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-  menuMove: string = 'left';
   constructor(private loginService: LoginService, private router: Router) {
   }
 
   ngOnInit() {
-  }
-  toggleMove() {
-    this.menuMove = (this.menuMove === 'left') ? 'right' : 'left';
   }
 
   logout() {
