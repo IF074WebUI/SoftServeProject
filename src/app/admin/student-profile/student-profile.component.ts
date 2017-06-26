c18efcb84a8907610014d21128fe0cb6399e4c0d
Danylo Antoniuk <ldominiquel@i.ua>
2017-06-26 12:52:43 +0300

    editing student works with dynamic-form

--- src/app/admin/student-profile/student-profile.component.ts
+++ src/app/admin/student-profile/student-profile.component.ts
@@ -3,28 +3,26 @@ import { Student } from '../students/student';
 import { StudentsService } from '../students/students.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { GetRecordsByIdService } from '../services/get-records-by-id.service';
-import { FormControl, FormGroup, Validators} from '@angular/forms';
 import { Group } from '../group/group';
 import { GetAllRecordsService } from '../services/get-all-records.service';
 import {SpinnerService} from '../universal/spinner/spinner.service';
 import {ToastsManager} from 'ng2-toastr';
-import {DynamicFormComponent} from "../universal/dynamic-form/container/dynamic-form/dynamic-form.component";
-import {STUDENT_CONFIG} from "../universal/dynamic-form/config";
+import {DynamicFormComponent} from '../universal/dynamic-form/container/dynamic-form/dynamic-form.component';
+import {STUDENT_CONFIG} from '../universal/dynamic-form/config';
+
 @Component({
   selector: 'dtester-student-profile',
   templateUrl: './student-profile.component.html',
   styleUrls: ['./student-profile.component.scss'],
   providers: [StudentsService, GetRecordsByIdService, GetAllRecordsService]
 })
+
 export class StudentProfileComponent implements OnInit {
-  MODAL_REQUIRED = 'Поле обов\'язкове до заповнення';
   groups: Group[];
   user_id: number;
   student: Student;
   AdminUser: Student;
   studentForEdit: Student;
-  studentEditForm: FormGroup;
-  studentForEdit_password: string;
   studentEditData = {};
 
   @ViewChild(DynamicFormComponent) popup: DynamicFormComponent;
@@ -44,32 +42,8 @@ export class StudentProfileComponent implements OnInit {
     this.getStudent();
     this.getAdminUser();
     this.getGroups();
-
-    this.studentEditForm = new FormGroup({
-      'group_id': new FormControl(''),
-      'student_surname': new FormControl('', Validators.required),
-      'student_name': new FormControl('', Validators.required),
-      'student_fname': new FormControl('', Validators.required),
-      'gradebook_id': new FormControl('', Validators.required),
-      'username': new FormControl('', Validators.required),
-      'password': new FormControl('', Validators.required),
-      'email': new FormControl('', Validators.required)
-    });
   }
 
-  changeListener($event): void {
-    this.readThis($event.target);
-  }
-
-  readThis(inputValue: any): void {
-    const file: File = inputValue.files[0];
-    const myReader: FileReader = new FileReader();
-
-    myReader.onloadend = (e) => {
-      this.studentForEdit.photo = myReader.result;
-    };
-    myReader.readAsDataURL(file);
-  }
   getStudent() {
     this.spinner.showSpinner();
     this.studentsService.getStudentById(this.user_id).subscribe((resp: Student) => {
@@ -78,26 +52,20 @@ export class StudentProfileComponent implements OnInit {
    });
    }
 
-  edit(student: any) {
-    // this.configs[1]['action'] = 'edit';
-    this.popup.sendItem(student);
-    this.popup.showModal();
-  }
-
    selectedStudent(student: Student, AdminUser) {
     this.studentForEdit = student;
     this.studentForEdit.username = AdminUser.username;
     this.studentForEdit.email = AdminUser.email;
-    this.studentEditForm.setValue({
-      group_id: this.studentForEdit.group_id,
-      student_name: this.studentForEdit.student_name,
-      student_surname: this.studentForEdit.student_surname,
-      student_fname: this.studentForEdit.student_fname,
-      gradebook_id: this.studentForEdit.gradebook_id,
-      username: this.studentForEdit.username,
-      password: this.studentForEdit.plain_password,
-      email: this.studentForEdit.email
-     });
+    this.popup.sendItem({
+      'student_name': this.studentForEdit.student_name,
+      'student_surname': this.studentForEdit.student_surname,
+      'student_fname': this.studentForEdit.student_fname,
+      'gradebook': this.studentForEdit.gradebook_id,
+      'email': this.studentForEdit.email,
+      'group': this.studentForEdit.group_name,
+      'group_id': this.studentForEdit.group_id
+    }, 'Student', null, this.studentForEdit.photo);
+    this.popup.showModal();
    }
 
   getAdminUser() {
@@ -122,21 +90,23 @@ export class StudentProfileComponent implements OnInit {
 
   getStudentsWithGroupName(data) {
     this.student = data;
-    // console.log(this.student);
     this.getRecordsByIdService.getRecordsById('group', this.student.group_id).subscribe((StudentData) => {
       this.student.group_name = StudentData[0].group_name;
     });
   }
 
-  formSubmitted() {
+  formSubmitted(value) {
     this.studentEditData = {
-      'password_confirm' : this.studentForEdit_password,
-      'plain_password': this.studentForEdit_password
+      'username': this.studentForEdit.username,
+      'password': this.studentForEdit.plain_password,
+      'password_confirm' : this.studentForEdit.plain_password,
+      'plain_password': this.studentForEdit.plain_password,
     };
-    this.studentsService.update(this.studentEditForm.value, this.studentForEdit.photo, this.studentEditData, this.user_id)
+    this.studentsService.update(value, this.studentEditData, this.user_id)
       .subscribe(resp => {
         this.getStudent();
         this.getAdminUser();
+        this.popup.cancel();
         this.toastr.success(`Студент ${this.studentForEdit['student_name']} ${this.studentForEdit['student_surname']}
         ${this.studentForEdit['student_fname']} успішно відредагований`);
       }, error2 => {
