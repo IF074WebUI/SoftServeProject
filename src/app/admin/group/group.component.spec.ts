b5da327ba2032ef9bc3bdfa9df652c7b30ad13c9
Mykola <pereslop@gmail.com>
2017-06-27 12:32:37 +0300

    first two tests

--- src/app/admin/group/group.component.spec.ts
+++ src/app/admin/group/group.component.spec.ts
@@ -1,19 +1,28 @@
 import {TestBed, inject, ComponentFixture} from '@angular/core/testing';
 // import {HttpModule} from "@angular/http";
-// import {GroupService} from "./group.service";
+import {GroupService} from "./group.service";
 // import {DebugElement} from "@angular/core";
 import { GroupComponent } from './group.component';
 
 describe(`Component: Group Component`, () => {
-  // let component: GroupComponent;
-  fit (`button class must be 'fa fa-calendar'`, () => {
-    expect(1 + 1).toEqual(2);
-  });
+    let component: GroupComponent;
+    let fakeGroupService: any;
+    beforeEach(() => {
+      fakeGroupService = {
 
-  // fit (`button class must be 'fa fa-calendar'`, () => {
-  //   const component = new GroupComponent(null);
-  //   expect(component.btnClass).toEqual('fa fa-calendar');
-  // });
+      };
+      component = new GroupComponent(null);
+    })
+    fit('shouldv 1+1', () => {
+      expect(1 + 1).toEqual(2);
+    });
+    fit(`it should have a button class 'fa fa-calendar'`, () => {
 
+      expect(component.btnClass).toEqual('fa fa-calendar');
+    });
+    fit(`should set pageNumber to 2`, () => {
+      component.ngOnInit();
+      component.changePage(2);
+      expect(component.pageNumber).toEqual(2);
+    });
 });
-
