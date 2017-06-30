b3f69f14028bdfc57024e7cfaa46d5a6fe301532
Mykola <pereslop@gmail.com>
2017-06-30 11:26:11 +0300

    add logout button Merge branch 'developing' of https://github.com/IF074WebUI/SoftServeProject into developingGroups
    # Conflicts:
    #	src/app/student/student.module.ts
    #	src/app/student/student.routes.ts

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
+import {LoginService} from "../login/login.service";
+import {Router} from "@angular/router";
 @Component({
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent {
+
+constructor(private loginService: LoginService, private router: Router) {
+}
+
+ngOnInit() {
+}
+
+logout() {
+  this.loginService.logout().subscribe(response => {
+    this.router.navigate(['/login']);
+  });
+}
 }
