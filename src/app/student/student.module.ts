3dae03743bf95674db8428b287fcc95be3058497
Mykola <pereslop@gmail.com>
2017-06-30 11:22:16 +0300

    Merge branch 'developing' of https://github.com/IF074WebUI/SoftServeProject into developingGroups
    # Conflicts:
    #	src/app/student/student.module.ts
    #	src/app/student/student.routes.ts

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { studentRoutes } from './student.routes';
 import { StudentComponent } from './student.component';
-import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {TestPlayerComponent} from "./test-player/test-player.component";
-
-import { StudentsMainPageComponent } from './students-main-page/students-main-page.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TestPlayerComponent} from './test-player/test-player.component';
 @NgModule({
-  declarations: [StudentComponent, TestPlayerComponent, StudentsMainPageComponent],
+  declarations: [StudentComponent, TestPlayerComponent],
   imports: [RouterModule.forChild(studentRoutes)]
 })
 export class StudentModule {
