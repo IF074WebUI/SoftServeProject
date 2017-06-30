dea030967124f7e43aa4761ec8fcf8665ab60b57
Mykola <pereslop@gmail.com>
2017-06-30 11:03:49 +0300

    srudent main

--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { studentRoutes } from './student.routes';
 import { StudentComponent } from './student.component';
+import { StudentsMainPageComponent } from './students-main-page/students-main-page.component';
 @NgModule({
-  declarations: [StudentComponent],
+  declarations: [StudentComponent, StudentsMainPageComponent],
   imports: [RouterModule.forChild(studentRoutes)]
 })
 export class StudentModule {
--- src/app/student/student.routes.ts
+++ src/app/student/student.routes.ts
@@ -1,5 +1,8 @@
 import { Route } from '@angular/router';
 import { StudentComponent } from './student.component';
+import {StudentsMainPageComponent} from "./students-main-page/students-main-page.component";
 export const studentRoutes: Route[] = [
-  { path: '', component: StudentComponent, pathMatch: 'full' }
+  {path: '', redirectTo: 'studentMain', pathMatch: 'full'},
+  { path: '', component: StudentComponent},
+  { path:  'studentMain', component: StudentsMainPageComponent}
 ];
