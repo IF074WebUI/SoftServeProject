059be6a28d495df44b40fe83721128b79eca78ac
Elena Taras <elena_taras@ukr.net>
2017-07-01 20:58:41 +0300

    after Mycola's merge

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -2,17 +2,14 @@ import {Component, OnInit} from '@angular/core';
 import {LoginService} from '../login/login.service';
 import {Router} from '@angular/router';
 import {StudentsService} from '../admin/students/students.service';
-import {ResultsService} from '../admin/services/results.service';
-
-import { StudentsService } from '../admin/students/students.service';
-import { Observable } from 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/forkJoin';
 import 'rxjs/add/operator/map';
 import {ResultsService} from "../admin/services/results.service";
 import {ToastsManager} from "ng2-toastr";
 import {SpinnerService} from "../admin/universal/spinner/spinner.service";
 import {TimetableService} from "../admin/timetable/timetable.service";
-import { StudentParameters } from './StudentParameters';
+import {StudentParameters} from './StudentParameters';
 import {SubjectService} from "../admin/subject/subject.service";
 import {TestsService} from "../admin/services/tests.service";
 import {QuestionsService} from "../admin/services/questions.service";
@@ -24,13 +21,13 @@ import {DeleteRecordByIdService} from "../admin/services/delete-record-by-id.ser
   styleUrls: ['./student.component.scss']
 })
 export class StudentComponent implements OnInit {
-studentId: number;
-studentFullName: string;
-objLoaderStatus: boolean;
-noTests: string;
-noRecordsResponce: string;
-checkTestAvailability: boolean;
-result: StudentParameters;
+  studentId: number;
+  studentFullName: string;
+  objLoaderStatus: boolean;
+  noTests: string;
+  noRecordsResponce: string;
+  checkTestAvailability: boolean;
+  result: StudentParameters;
 
   constructor(private loginService: LoginService,
               private router: Router,
@@ -44,80 +41,80 @@ result: StudentParameters;
               private question: QuestionsService,
               private answer: AnswersService,
               private teestDetail: TestDetailService,
-              private deleteRecords: DeleteRecordByIdService
-  ) {
+              private deleteRecords: DeleteRecordByIdService) {
     this.objLoaderStatus = false;
     this.noTests = 'Немає доступних тестів';
     this.noRecordsResponce = 'no records';
     this.checkTestAvailability = false;
   }
 
-ngOnInit() {
-  this.spinner.loaderStatus.subscribe((val: boolean) => {
-    this.objLoaderStatus = val;
-  });
-this.getStudentId();
-this.getTests();
-}
-
-getStudentId() {
-  this.spinner.showSpinner();
-  this.studentId = +window.sessionStorage.getItem('studentId');
-  this.loginService.checkLogged()
-      .subscribe(
-        res => { this.spinner.hideSpinner(); },
-        err => this.toastr.error(err)
-      );
-}
-getTests() {
-  let test = this.test.getTestsBySubject(this.result.subject[0].subject_id)
-  let timeTable = this.timeTable.getTimeTablesForGroup(this.result.student['group_id']);
-  this.studentService.getStudentById(+this.studentId).subscribe(res => {
-    this.result['student'] = res[0];
-    console.log(this.result['student'])
-    Observable.forkJoin([timeTable, test]).subscribe(results => {
-      this.result['groupId'] = +results[0][0]['group_id'];
-      console.log(this.result['groupId']);
+  ngOnInit() {
+    this.spinner.loaderStatus.subscribe((val: boolean) => {
+      this.objLoaderStatus = val;
     });
-  });
-
-
-}
-getTestForStudent() {
-  this.spinner.showSpinner();
-  this.studentService.getStudentById(this.studentId)
-    .subscribe(
-      res => {
-        this.studentFullName = res[0]['student_surname'] + ' ' + res[0]['student_name'];
-        this.timeTable.getTimeTablesForGroup(res.group_id)
-          .subscribe( resTimeTable => {
-            if (resTimeTable.response === this.noRecordsResponce ) {
-              this.checkTestAvailability = true;
-              console.log(resTimeTable);
-              this.spinner.hideSpinner();
-            } else {
-              console.log('somesing went wrong :)');
-              this.spinner.hideSpinner();
-            }
-          });
-      },
-      err => this.toastr.error(err),
-      () => {}
-    );
-}
-logout() {
-  this.loginService.logout().subscribe(() => {
-    this.router.navigate(['/login']);
-  });
-}
-  openTestPlayer() {
-    this.router.navigate(['./student/test-player']);
+    this.getStudentId();
+    this.getTests();
   }
 
+  getStudentId() {
+    this.spinner.showSpinner();
+    this.studentId = +window.sessionStorage.getItem('studentId');
+    this.loginService.checkLogged()
+      .subscribe(
+        res => {
+          this.spinner.hideSpinner();
+        },
+        err => this.toastr.error(err)
+      );
+  }
+
+  getTests() {
+    let test = this.test.getTestsBySubject(this.result.subject[0].subject_id)
+    let timeTable = this.timeTable.getTimeTablesForGroup(this.result.student['group_id']);
+    this.studentService.getStudentById(+this.studentId).subscribe(res => {
+      this.result['student'] = res[0];
+      console.log(this.result['student'])
+      Observable.forkJoin([timeTable, test]).subscribe(results => {
+        this.result['groupId'] = +results[0][0]['group_id'];
+        console.log(this.result['groupId']);
+      });
+    });
+
+
+  }
+
+  getTestForStudent() {
+    this.spinner.showSpinner();
+    this.studentService.getStudentById(this.studentId)
+      .subscribe(
+        res => {
+          this.studentFullName = res[0]['student_surname'] + ' ' + res[0]['student_name'];
+          this.timeTable.getTimeTablesForGroup(res.group_id)
+            .subscribe(resTimeTable => {
+              if (resTimeTable.response === this.noRecordsResponce) {
+                this.checkTestAvailability = true;
+                console.log(resTimeTable);
+                this.spinner.hideSpinner();
+              } else {
+                console.log('somesing went wrong :)');
+                this.spinner.hideSpinner();
+              }
+            });
+        },
+        err => this.toastr.error(err),
+        () => {
+        }
+      );
+  }
 
   logout() {
     this.loginService.logout().subscribe(() => {
       this.router.navigate(['/login']);
     });
   }
+
+  openTestPlayer() {
+    this.router.navigate(['./student/test-player']);
+  }
+
 }
