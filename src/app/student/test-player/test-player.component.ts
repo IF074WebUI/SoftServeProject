21da997a91c67243de56fde2f22b231045c990b6
Elena Taras <elena_taras@ukr.net>
2017-07-01 23:50:30 +0300

    test player styled

--- src/app/student/test-player/test-player.component.ts
+++ src/app/student/test-player/test-player.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {TestPlayerService} from './test-player.service';
-import {GetTestsBySubjectService} from "../../admin/services/get-tests-by-subject.service";
-import {Answer} from "../../admin/answers/answer";
-import {Observable} from "rxjs/Observable";
-import {TestDetail} from "../../admin/test-detail/testDetail";
+import {GetTestsBySubjectService} from '../../admin/services/get-tests-by-subject.service';
+import {Answer} from '../../admin/answers/answer';
+import {Observable} from 'rxjs/Observable';
+import {TestDetail} from '../../admin/test-detail/testDetail';
+import 'rxjs/add/observable/of';
+
 
 export class Object {
   public attempts: number;
@@ -46,69 +48,41 @@ export class Question {
 @Component({
   selector: 'app-test-player',
   templateUrl: './test-player.component.html',
-  styleUrls: ['./test-player.component.css'],
+  styleUrls: ['./test-player.component.scss'],
   providers: [GetTestsBySubjectService]
 })
 export class TestPlayerComponent implements OnInit {
   test_id: number;
-  subject_id;
-  Alltests: Object[] = [];
   test: Object;
-  current_test_id: number;
-  questions1: Array<Object>;
-  questions2: Array<Object>;
-  questions3: Array<Object>;
-
-  currentQuestion: Question;
+  questions: Question[] = [];
+  question: Question;
   start: boolean;
+  disable: boolean;
   i: number = 0 ;
   test_details: TestDetail[] = [];
-  level: number = 1;
+
+  NEXT_QUESTION = 'Наступне питання';
+  PREV_QUESTION = 'Попереднє питання';
 
   constructor(private test_player: TestPlayerService) {
   }
 
   ngOnInit() {
-    this.test_id = 13;
-    console.log(this.test_id);
+    this.test_id = 1;
     this.getTestDetails();
- //   this.subject_id = '2';
- //   this.getTestBySubject(this.subject_id);
   }
 
-  //
-  // getTestBySubject(id: number): void {
-  //   this.test_player.getTestDetatilBuTest(id).subscribe(resp => {
-  //     this.Alltests = <Object[]> resp;
-  //   });
-  // }
-
   getTestDetails(){
-    this.test_player.getTestDetail(this.test_id).subscribe(resp => {this.test_details = resp; console.log(this.test_details);
+    this.test_player.getTestDetail(this.test_id).subscribe(resp => {this.test_details = resp;
    });
   }
 
+
   startTest() {
-   // this.tasks = test.tasks;
-   // console.log(this.test_details[this.i].tasks);
-    this.test_player.getQuestionsByLevelRandom(this.test_id,  this.level,  1).subscribe(resp  => {this.questions1 = resp;
-    console.log(this.questions1)});
     this.start = true;
-    console.log(this.questions1);
-    this.test_player.getQuestionsByLevelRandom(this.test_id, 2,  6).subscribe(resp => this.questions2 = resp);
-    this.test_player.getQuestionsByLevelRandom(this.test_id, 3,  1).subscribe(resp => this.questions2 = resp);
-  }
+    this.test_player.getQuestions(this.test_details).subscribe(resp => {this.questions = resp;
+    console.log(resp)});
+   }
 
-  // ShowQuestionsByLevel(level: number){
-  //   console.log(this.test_details[this.i].tasks);
-  //   this.test_player.getQuestionsByLevelRandom(this.test_id, level,  this.test_details[this.i].tasks).subscribe(resp => this.questions2 = resp);
-  // }
-
-  // nextQuestion() {
-  //   if (this.i <= this.test_details.length){
-  //   ++this.i;
-  //   this.test_player.getQuestionsByLevelRandom(this.test_id, this.level,  this.test_details[this.i].tasks).subscribe(resp => this.questions = resp);
-  // } else {console.log('no questions'); }}
-  //
 
 }
