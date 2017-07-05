cd0ab32b6cae18fb1957d2db800b585d92ac4a4e
Mykola.Kaban <pereslop@gmail.com>
2017-07-06 00:17:32 +0300

    added styles for studentProfile, removed route to him, console messages deleted

--- src/app/student/student-profile/student-profile.component.ts
+++ src/app/student/student-profile/student-profile.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import {Student} from "../../admin/students/student";
-import {StudentsService} from "../../admin/students/students.service";
-import {GroupService} from "../../admin/group/group.service";
-import {Group} from "../../admin/group/group";
-import {ToastsManager} from "ng2-toastr";
-import {ActivatedRoute} from "@angular/router";
+import {Student} from '../../admin/students/student';
+import {StudentsService} from '../../admin/students/students.service';
+import {GroupService} from '../../admin/group/group.service';
+import {Group} from '../../admin/group/group';
+import {ToastsManager} from 'ng2-toastr';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'dtester-student-profile',
   templateUrl: './student-profile.component.html',
-  styleUrls: ['./student-profile.component.css']
+  styleUrls: ['./student-profile.component.scss']
 })
 export class StudentProfileComponent implements OnInit {
 
@@ -35,7 +35,7 @@ export class StudentProfileComponent implements OnInit {
           this.student = response[0];
           this.groupService.getGroupById(this.student.group_id)
             .subscribe(
-              groupResponce => { this.group = groupResponce[0]; },
+              groupResponce => { this.group = groupResponce[0]; console.log(this.student); },
               error => this.toastr.error(error)
             );
         }, error => this.toastr.error(error)
